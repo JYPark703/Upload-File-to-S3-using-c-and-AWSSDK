dcd8fdb949d8cb58f38792bb18b2d99b709aee26
JYPark703 <jinjin401703@gmail.com>
2022-02-10 20:31:29 -0800

    first commit

---
+++ ReadMe_Demo_Upload File to S3 using c# and AWSSDK.txt
@@ -0,0 +1,26 @@
+-----------------------------------------README------------------------------------------------
+===Upload File to S3 using c# and AWSSDK===
+Group10
+Name: Jin Young Park 
+
+To demonstrate this program, I need to reveal my aws access key, my aws id, and password, so I submit my demo video.
+===========
+ASW S3 is Blobs and has direct access to the API. And Amazon uses ASWADK to help us use them. I used this to create a program that uploads files to S3 using C#, ASWADK to access the API directly.
+
+===Usage===
+1. To access the ASW as an API, you need an access key. Create this key in your ASW account and save it in a text file with the folliwing format:
+[default]
+aws_access_key_id={Your key id}
+aws_secret_access_key={Your secret key}
+
+2. Then modify the App.config:
+    <add key="AWSProfilesLocation" value="{your key storage file path}"/>
+    <add key="AWSRegion" value="{your s3 region}"/>
+
+3. And also modify the Program.cs:
+	static string bucketName = "{Your bucket name}";
+        static string folderName = "{Folder name in bucket where you want to save image}";
+        static string keyName = "{File name you want to save}";
+        static string filePath = @"{The location where you want to save the file}";
+
+4. Running
---
+++ demojw003/demojw003/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+
+namespace demojw003
+{
+    class Program
+    {
+        static string bucketName = "file.jydemohw003.com";
+        static string folderName = "ImageFiles";
+        static string keyName = "sample2.png";
+        static string filePath = @"C:\amazon_s3\Images\sample2.png";
+        
+
+        static IAmazonS3 client;
+
+        static void Main(string[] args)
+        {
+            using (client= new AmazonS3Client(Amazon.RegionEndpoint.USWest2))
+            {
+                SaveImage();
+            }
+        }
+        static void SaveImage()
+        {
+            try
+            {
+                TransferUtility fileTransferUtility = new TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.USWest2));
+                TransferUtilityUploadRequest request = new TransferUtilityUploadRequest()
+                {
+                    BucketName = string.Format(@"{0}/{1}", bucketName, folderName), Key = keyName, FilePath = filePath,
+                };
+                fileTransferUtility.Upload(request);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
